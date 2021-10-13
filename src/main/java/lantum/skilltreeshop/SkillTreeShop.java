cebd8e737278d7f83083cc41870b1d6c8981c0c8
Kim JinWoo <kjinwoo12@gmail.com>
2021-10-13 16:21:53 +0900

    Init commit

---
+++ src/main/java/lantum/skilltreeshop/SkillTreeShop.java
@@ -0,0 +1,17 @@
+package lantum.skilltreeshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkillTreeShop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
